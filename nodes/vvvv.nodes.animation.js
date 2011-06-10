e744aae2585bf89f2a2c2257d50405965a3db904
zauner <sagishi@zive.at>
2011-06-10 15:12:08 +0200

    added LinearFilter (Animation)

--- nodes/vvvv.nodes.animation.js
+++ nodes/vvvv.nodes.animation.js
@@ -51,3 +51,59 @@ VVVV.Nodes.LFO = function(id, graph) {
 
 }
 VVVV.Nodes.LFO.prototype = new VVVV.Core.Node();
+
+
+
+VVVV.Nodes.LinearFilter = function(id, graph) {
+  this.constructor(id, "LinearFilter (Animation)", graph);
+  
+  var positionIn = this.addInputPin("Go To Position", [0.0], this);
+  var filterTimeIn = this.addInputPin("FilterTime", [1.0], this);
+  
+  var positionOut = this.addOutputPin("Position Out", [0.0], this);
+  var velocityOut = this.addOutputPin("Velocity Out", [0.0], this);
+  var accelerationOut = this.addOutputPin("Acceleration Out", [0.0], this);
+  
+  var lastUpdate = new Date().getTime();
+  var currPos = [];
+  var velocity = [];
+
+  this.evaluate = function() {
+    
+    var maxSize = this.getMaxInputSliceCount();
+    var pinsChanged = positionIn.pinIsChanged() || filterTimeIn.pinIsChanged();
+    var dt = new Date().getTime()-lastUpdate;
+    
+    for (var i=0; i<maxSize; i++) {
+      if (currPos[i]==undefined)
+        currPos[i] = 0.0;
+      var targetPos = parseFloat(positionIn.getValue(i));
+      var filterTime = parseFloat(filterTimeIn.getValue(i));
+        
+      if (pinsChanged) {
+        console.log(this.inputPins["Go To Position"]);
+        console.log(positionIn);
+        dt = 0;
+        
+        if (filterTime>0)
+          velocity[i] = (targetPos-currPos[i])/(filterTime*1000);
+        else
+          velocity[i] = 0;
+        console.log(velocity[i]);
+      
+      }
+      
+      if (currPos[i]>=targetPos)
+          return;
+      
+      currPos[i] += velocity[i]*dt;
+      
+      positionOut.setValue(i, currPos[i]);
+      
+    }
+    
+    lastUpdate = new Date().getTime();
+  }
+
+}
+VVVV.Nodes.LinearFilter.prototype = new VVVV.Core.Node();
