8a7a59e712157884c808f7671a5d8975eecbd685
zauner <sagishi@zive.at>
2011-06-30 11:13:32 +0200

    changed MainLoop API. there will now be multiple instances of MainLoop created with 'new', as well es methods for running and stopping each individually

--- mainloop/vvvv.mainloop.js
+++ mainloop/vvvv.mainloop.js
@@ -1,27 +1,37 @@
 
-VVVV.Core.MainLoop = {
-
-  run: function(graph) {
+VVVV.Core.MainLoop = function(graph) {
   
-    var fps = 30;
-    var framecount = 0;
-    var dom = new VVVV.Core.DOMInterface(graph);
-    
-    function update() {
-      framecount ++;
-      var start = new Date().getTime();
-      dom.populateIOBoxes();
-      graph.evaluate();
-      dom.processOutputIOBoxes();
-      var elapsed = new Date().getTime()-start;
-      window.status = elapsed;
-      //if (framecount<1)
-        window.setTimeout(update, Math.max(0, Math.round(1000/fps-elapsed)));
-    }
-    
-    update();
+  var fps = 30;
+  var framecount = 0;
+  var dom = new VVVV.Core.DOMInterface(graph);
+  var run = true;
   
-  
+  function update() {
+    framecount ++;
+    var start = new Date().getTime();
+    dom.populateIOBoxes();
+    graph.evaluate();
+    dom.processOutputIOBoxes();
+    var elapsed = new Date().getTime()-start;
+    window.status = elapsed;
+    if (run)
+      window.setTimeout(update, Math.max(0, Math.round(1000/fps-elapsed)));
   }
+  
+  this.stop = function() {
+    run = false;
+  }
+  
+  this.start = function() {
+    run = true;
+    update();
+  }
+  
+  this.isRunning = function() {
+    return run;
+  }
+  
+  update();
+  
 
 }
