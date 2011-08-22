a96468e54cdd9fa9fed3c8874e70f343f6847a36
zauner <sagishi@zive.at>
2011-08-22 14:59:56 +0200

    adds Complex Group with Abs (Complex)

---
+++ nodes/vvvv.nodes.complex.js
@@ -0,0 +1,54 @@
+// VVVV.js -- Visual Web Client Programming
+// (c) 2011 Matthias Zauner
+// VVVV.js is freely distributable under the MIT license.
+// Additional authors of sub components are mentioned at the specific code locations.
+
+
+/*
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ NODE: Abs (Complex)
+ Author(s): Matthias Zauner
+ Original Node Author(s): VVVV Group
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+*/
+
+VVVV.Nodes.Abs = function(id, graph) {
+  this.constructor(id, "Abs (Complex)", graph);
+  
+  this.meta = {
+    authors: ['Matthias Zauner'],
+    original_authors: ['VVVV Group'],
+    credits: [],
+    compatibility_issues: []
+  };
+  
+  var inputRealIn = this.addInputPin("Input Real", [0.0], this);
+  var inputImagIn = this.addInputPin("Input Imagiary", [0.0], this);
+  
+  var outputOut = this.addOutputPin("Output", [0.5], this);
+
+  this.evaluate = function() {
+    var maxSize = this.getMaxInputSliceCount();
+    
+    for (var i=0; i<maxSize; i++) {
+      outputOut.setValue(i,
+        Math.sqrt(
+          Math.pow(parseFloat(inputRealIn.getValue(i)), 2) +
+          Math.pow(parseFloat(inputImagIn.getValue(i)), 2)
+        )
+      );
+    }
+    outputOut.setSliceCount(maxSize);
+  }
+
+}
+VVVV.Nodes.Abs.prototype = new VVVV.Core.Node();
+
+
+
+
+
+
+
+
+
