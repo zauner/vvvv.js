29436b501af97364f46b0d9494ca4a9c61fafa81
zauner <sagishi@zive.at>
2011-09-05 10:59:44 +0200

    adds ctrl+alt+f shortcut to print framerate

--- mainloop/vvvv.mainloop.js
+++ mainloop/vvvv.mainloop.js
@@ -6,6 +6,9 @@ VVVV.Core.MainLoop = function(patch) {
   var dom = new VVVV.Core.DOMInterface(patch);
   var run = true;
   
+  var measureStart = 0;
+  var print_framerate = false;
+  
   function update() {
     framecount ++;
     var start = new Date().getTime();
@@ -13,7 +16,13 @@ VVVV.Core.MainLoop = function(patch) {
     patch.evaluate();
     dom.processOutputConnectors();
     var elapsed = new Date().getTime()-start;
-    window.status = elapsed;
+    if (framecount%10 == 0) {
+      if (print_framerate) {
+        console.log((1 / ((start-measureStart)/(1000*10)))+'fps');
+        print_framerate = false;
+      }
+      measureStart = start;
+    }
     if (run) // && framecount<=150)
       window.setTimeout(update, Math.max(0, Math.round(1000/fps-elapsed)));
   }
@@ -32,6 +41,13 @@ VVVV.Core.MainLoop = function(patch) {
   }
   
   update();
+  
+  $(window).keydown(function(e) {
+  
+    // ctrl + alt + F to print framerate
+    if ((e.which==102 || e.which==70) && e.altKey && e.ctrlKey)
+      print_framerate = true;
+  });
   
 
 }
