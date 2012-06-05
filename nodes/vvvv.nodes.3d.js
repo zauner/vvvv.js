fbc086ea1d1c8c4a90df10ad7a38fa78678dcfef
zauner <sagishi@zive.at>
2012-06-05 15:06:22 +0200

    adds Cartesian (3d)

--- nodes/vvvv.nodes.3d.js
+++ nodes/vvvv.nodes.3d.js
@@ -50,8 +50,65 @@ VVVV.Nodes.Polar3d = function(id, graph) {
       lengthOut.setValue(i, len);
     }
     
+    yawOut.setSliceCount(maxSize);
+    pitchOut.setSliceCount(maxSize);
     lengthOut.setSliceCount(maxSize);
   }
 
 }
 VVVV.Nodes.Polar3d.prototype = new VVVV.Core.Node();
+
+
+/*
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ NODE: Cartesian (3d)
+ Author(s): 'Matthias Zauner'
+ Original Node Author(s): 'VVVV Group'
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+*/
+
+VVVV.Nodes.Cartesian3d = function(id, graph) {
+  this.constructor(id, "Cartesian (3d)", graph);
+  
+  this.meta = {
+    authors: ['Matthias Zauner'],
+    original_authors: ['VVVV Group'],
+    credits: [],
+    compatibility_issues: []
+  };
+  
+  this.auto_evaluate = false;
+  
+  // input pins
+  var pitchIn = this.addInputPin('Pitch', [0], this);
+  var yawIn = this.addInputPin('Yaw', [0], this);
+  var lengthIn = this.addInputPin('Length', [1], this);
+
+  // output pins
+  var xOut = this.addOutputPin('X', [0], this);
+  var yOut = this.addOutputPin('Y', [0], this);
+  var zOut = this.addOutputPin('Z', [1], this);
+
+  
+  this.evaluate = function() {
+    // to implement; maybe start with something like this:
+    
+    var maxSize = this.getMaxInputSliceCount();
+    
+    for (var i=0; i<maxSize; i++) {
+      var pitch = (pitchIn.getValue(i) + 0.25) * 2 * Math.PI;
+      var yaw = yawIn.getValue(i) * 2 * Math.PI;
+      var length = lengthIn.getValue(i);
+      
+      zOut.setValue(i, -length * Math.cos(yaw) * Math.sin(pitch));
+      xOut.setValue(i, -length * Math.sin(yaw) * Math.sin(pitch));
+      yOut.setValue(i, -length * Math.cos(pitch));
+    }
+    
+    xOut.setSliceCount(maxSize);
+    yOut.setSliceCount(maxSize);
+    zOut.setSliceCount(maxSize);
+  }
+
+}
+VVVV.Nodes.Cartesian3d.prototype = new VVVV.Core.Node();
