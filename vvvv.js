68abd93adbc784bc8dbc00085aa03cfa03768cfd
Claudius Coenen <coenen@meso.net>
2012-07-06 14:25:25 +0200

    reworked initialisation. The scripts show up in Chrome now Issue #6

--- vvvv.js
+++ vvvv.js
@@ -19,76 +19,109 @@ if(!window.console) {
 }
 
 // actual VVVV.js initialization code
-VVVV = {}
+VVVV = {};
+VVVV.Nodes = {};
+
 VVVV.onNotImplemented = function(nodename) {
   console.log("Warning: "+nodename+" is not implemented.");
-}
+};
 
-function initVVVV(path_to_vvvv, mode) {
+/**
+ * Adds the neccessary JavaScripts to the head, calls the callback once everything is in place.
+ * @param {String} path_to_vvvv points to the folder of your vvvv.js
+ * @param {String} mode. Can be either "full", "vvvviewer" or "run". Depends on what you want to do 
+ * @param {Function} callback will be called once all the scripts and initialisations have been finished.
+ */
+VVVV.init = function (path_to_vvvv, mode, callback) {
 
   console.log('loading vvvv.js ...');
 
+  var head = document.getElementsByTagName('head')[0];
+  var script = document.createElement( 'script' );
+  var loadCounter = 0;
+  script.setAttribute('language', 'JavaScript');
+
+  function loadMonitor(event) {
+    event.target.removeEventListener('load');
+    if (--loadCounter <= 0) {
+      initialisationComplete();
+    };
+    
+  }
+
+  function insertJS(url) {
+    var scriptClone;
+    scriptClone = script.cloneNode();
+    scriptClone.src = url;
+    scriptClone.addEventListener('load', loadMonitor);
+    loadCounter++;
+    head.appendChild(scriptClone);
+  }
+
   if ($('script[src*=underscore]').length==0)
-    $('head').append($('<script language="JavaScript" src="'+path_to_vvvv+'/lib/underscore/underscore-min.js"></script>'));
+    insertJS(path_to_vvvv+'/lib/underscore/underscore-min.js');
   if ($('script[src*="d3.js"]').length==0 && (mode=='full' || mode=='vvvviewer'))
-    $('head').append($('<script language="JavaScript" src="'+path_to_vvvv+'/lib/d3-v1.14/d3.min.js"></script>'));
+    insertJS(path_to_vvvv+'/lib/d3-v1.14/d3.min.js');
   if ($('script[src*=glMatrix]').length==0 && (mode=='full' || mode=='run'))
-    $('head').append($('<script language="JavaScript" src="'+path_to_vvvv+'/lib/glMatrix-0.9.5.min.js"></script>'));
+    insertJS(path_to_vvvv+'/lib/glMatrix-0.9.5.min.js');
 
   if ($('script[src*="vvvv.core.js"]').length==0) {
     $('head').append($('<script language="JavaScript" src="'+path_to_vvvv+'/core/vvvv.core.js"></script>'));
-    $('head').append($('<script language="JavaScript" src="'+path_to_vvvv+'/core/vvvv.core.vvvvconnector.js"></script>'));
+    insertJS(path_to_vvvv+'/core/vvvv.core.vvvvconnector.js');
     if (mode=='run' || mode=='full') {
-      $('head').append($('<script language="JavaScript" src="'+path_to_vvvv+'/mainloop/vvvv.mainloop.js"></script>'));
-      $('head').append($('<script language="JavaScript" src="'+path_to_vvvv+'/mainloop/vvvv.dominterface.js"></script>'));
-    
-      $('head').append($('<script language="JavaScript" src="'+path_to_vvvv+'/nodes/vvvv.nodes.value.js"></script>'));
-      $('head').append($('<script language="JavaScript" src="'+path_to_vvvv+'/nodes/vvvv.nodes.string.js"></script>'));
-      $('head').append($('<script language="JavaScript" src="'+path_to_vvvv+'/nodes/vvvv.nodes.boolean.js"></script>'));
-      $('head').append($('<script language="JavaScript" src="'+path_to_vvvv+'/nodes/vvvv.nodes.color.js"></script>'));
-      $('head').append($('<script language="JavaScript" src="'+path_to_vvvv+'/nodes/vvvv.nodes.spreads.js"></script>'));
-      $('head').append($('<script language="JavaScript" src="'+path_to_vvvv+'/nodes/vvvv.nodes.animation.js"></script>'));
-      $('head').append($('<script language="JavaScript" src="'+path_to_vvvv+'/nodes/vvvv.nodes.network.js"></script>'));
-      $('head').append($('<script language="JavaScript" src="'+path_to_vvvv+'/nodes/vvvv.nodes.system.js"></script>'));
-      $('head').append($('<script language="JavaScript" src="'+path_to_vvvv+'/nodes/vvvv.nodes.canvas.js"></script>'));
-      $('head').append($('<script language="JavaScript" src="'+path_to_vvvv+'/nodes/vvvv.nodes.html5.js"></script>'));
-      $('head').append($('<script language="JavaScript" src="'+path_to_vvvv+'/nodes/vvvv.nodes.transform.js"></script>'));
-      $('head').append($('<script language="JavaScript" src="'+path_to_vvvv+'/nodes/vvvv.nodes.vectors.js"></script>'));
-      $('head').append($('<script language="JavaScript" src="'+path_to_vvvv+'/nodes/vvvv.nodes.webgl.js"></script>'));
-      $('head').append($('<script language="JavaScript" src="'+path_to_vvvv+'/nodes/vvvv.nodes.complex.js"></script>'));
-      $('head').append($('<script language="JavaScript" src="'+path_to_vvvv+'/nodes/vvvv.nodes.enumerations.js"></script>'));
-      $('head').append($('<script language="JavaScript" src="'+path_to_vvvv+'/nodes/vvvv.nodes.2d.js"></script>'));
-      $('head').append($('<script language="JavaScript" src="'+path_to_vvvv+'/nodes/vvvv.nodes.3d.js"></script>'));
+      insertJS(path_to_vvvv+'/mainloop/vvvv.mainloop.js');
+      insertJS(path_to_vvvv+'/mainloop/vvvv.dominterface.js');
+
+      insertJS(path_to_vvvv+'/nodes/vvvv.nodes.value.js');
+      insertJS(path_to_vvvv+'/nodes/vvvv.nodes.string.js');
+      insertJS(path_to_vvvv+'/nodes/vvvv.nodes.boolean.js');
+      insertJS(path_to_vvvv+'/nodes/vvvv.nodes.color.js');
+      insertJS(path_to_vvvv+'/nodes/vvvv.nodes.spreads.js');
+      insertJS(path_to_vvvv+'/nodes/vvvv.nodes.animation.js');
+      insertJS(path_to_vvvv+'/nodes/vvvv.nodes.network.js');
+      insertJS(path_to_vvvv+'/nodes/vvvv.nodes.system.js');
+      insertJS(path_to_vvvv+'/nodes/vvvv.nodes.canvas.js');
+      insertJS(path_to_vvvv+'/nodes/vvvv.nodes.html5.js');
+      insertJS(path_to_vvvv+'/nodes/vvvv.nodes.transform.js');
+      insertJS(path_to_vvvv+'/nodes/vvvv.nodes.vectors.js');
+      insertJS(path_to_vvvv+'/nodes/vvvv.nodes.webgl.js');
+      insertJS(path_to_vvvv+'/nodes/vvvv.nodes.complex.js');
+      insertJS(path_to_vvvv+'/nodes/vvvv.nodes.enumerations.js');
+      insertJS(path_to_vvvv+'/nodes/vvvv.nodes.2d.js');
+      insertJS(path_to_vvvv+'/nodes/vvvv.nodes.3d.js');
     }
     if (mode=='vvvviewer' || mode=='full') {
-      $('head').append($('<script language="JavaScript" src="'+path_to_vvvv+'/vvvviewer/vvvv.vvvviewer.js"></script>'));
+      insertJS(path_to_vvvv+'/vvvviewer/vvvv.vvvviewer.js');
     }
   }
-  
-  VVVV.Root = path_to_vvvv;
-  VVVV.NodeLibrary = {};
 
-  var p = new VVVV.Core.Patch('');
-  _(VVVV.Nodes).each(function(n) {
-    var x = new n(0, p);
-    console.log("Registering "+x.nodename);
-    VVVV.NodeLibrary[x.nodename.toLowerCase()] = n;
-  });
-  
-  console.log('done ...');
+  function initialisationComplete() {
+    VVVV.Root = path_to_vvvv;
+    VVVV.NodeLibrary = {};
 
-  VVVV.Patches = [];
-  VVVV.MainLoops = [];
-
-  $("script[language='VVVV']").each(function() {
-    var p = new VVVV.Core.Patch($(this).attr('src'), function() {
-      var m = new VVVV.Core.MainLoop(this);
-      VVVV.MainLoops.push(m);
+    var p = new VVVV.Core.Patch('');
+    _(VVVV.Nodes).each(function(n) {
+      var x = new n(0, p);
+      console.log("Registering "+x.nodename);
+      VVVV.NodeLibrary[x.nodename.toLowerCase()] = n;
     });
-    VVVV.Patches.push(p);
-  });
-  
-}
+
+    console.log('done ...');
+
+    VVVV.Patches = [];
+    VVVV.MainLoops = [];
+
+    $("script[language='VVVV']").each(function() {
+      var p = new VVVV.Core.Patch($(this).attr('src'), function() {
+        var m = new VVVV.Core.MainLoop(this);
+        VVVV.MainLoops.push(m);
+      });
+      VVVV.Patches.push(p);
+    });
+
+    if (typeof callback === 'function') callback.call();
+  }
+};
 
 
 
