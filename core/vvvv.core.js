zauner/vvvv.js

commit 550573c601a421d9b8e05c6a7034e71d10585dd6

fixes interpretation of how spreads are stored in XML

core/vvvv.core.js
@@ -329,11 +329,30 @@ VVVV.Core = {
     function splitValues(v) {
       if (v==undefined)
         return [];
-      if (/\|/.test(v))
-        separator = "|";
-      else
-        separator = ",";
-      return v.split(separator).filter(function(d,i) { return d!=""});
+      if (this.vvvv_version<="45_26") { // legacy code
+        if (/\|/.test(v))
+          separator = "|";
+        else
+          separator = ",";
+        return v.split(separator).filter(function(d,i) { return d!=""});
+      }
+      
+      var result = [];
+      var currSlice = '';
+      var insideValue = false;
+      var len = v.length;
+      for (var i=0; i<len; i++) {
+        if (v[i]==',' && !insideValue) {
+          result.push(currSlice);
+          currSlice = '';
+        }
+        else if (v[i]=='|')
+          insideValue = !insideValue;
+        else
+          currSlice += v[i];
+      }
+      result.push(currSlice);
+      return result;
     }
     
     if (this.vvvv_version<="45_26") {

commit 22e4e201c740d5f9971be5f9a2238628ec75447a

fixes issue caused by 550573c601a421d9b8e, where not connected complex input pins got [] rather than their default value

core/vvvv.core.js
@@ -492,7 +492,12 @@ VVVV.Core = {
         // PINS
         $(this).find('pin').each(function() {
           var pinname = $(this).attr('pinname');
-          var values = splitValues($(this).attr('values'));
+          // this handles the case when complex input pins have a value of "||" when not connected.
+          // this should result in [] rather than in [""] as it would be correct for String input pins 
+          if (n.inputPins[pinname] && n.inputPins[pinname].reset_on_disconnect && $(this).attr('values')=="||")
+            values = undefined;
+          else
+            values = splitValues($(this).attr('values'));
 		  
           //Get all defaults from xml
           if (n.defaultPinValues[pinname] == undefined) {