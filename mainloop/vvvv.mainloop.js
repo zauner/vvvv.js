bc9f9745e295ca780e301db80d333f513f4fd712
zauner <sagishi@zive.at>
2013-10-04 10:44:02 +0200

    makes mainloop's framecount only count up when all resources are loaded

--- mainloop/vvvv.mainloop.js
+++ mainloop/vvvv.mainloop.js
@@ -10,21 +10,24 @@ VVVV.Core.MainLoop = function(patch, frames_per_second) {
   var print_framerate = false;
   
   function update() {
-    framecount ++;
-    var start = new Date().getTime();
-    dom.populateInputConnectors();
-    patch.evaluate();
-    dom.processOutputConnectors();
-    var elapsed = new Date().getTime()-start;
-    if (framecount%10 == 0) {
-      if (print_framerate) {
-        console.log((1 / ((start-measureStart)/(1000*10)))+'fps');
-        print_framerate = false;
+    if (patch.resourcesPending==0) {
+      framecount ++;
+      var start = new Date().getTime();
+      dom.populateInputConnectors();
+      patch.evaluate();
+      dom.processOutputConnectors();
+      var elapsed = new Date().getTime()-start;
+      if (framecount%10 == 0) {
+        if (print_framerate) {
+          console.log((1 / ((start-measureStart)/(1000*10)))+'fps');
+          print_framerate = false;
+        }
+        measureStart = start;
       }
-      measureStart = start;
     }
-    if (run) // && framecount<=150)
+    if (run) // && framecount<1)
       window.setTimeout(update, Math.max(0, Math.round(1000/fps-elapsed)));
+    
   }
   
   this.stop = function() {
@@ -44,7 +47,6 @@ VVVV.Core.MainLoop = function(patch, frames_per_second) {
   update();
   
   $(window).keydown(function(e) {
-  
     // ctrl + alt + F to print framerate
     if ((e.which==102 || e.which==70) && e.altKey && e.ctrlKey)
       print_framerate = true;
