zauner/vvvv.js

commit b6cdee2ed6570c192d7ff53a035f474b737c107e

adds black background to Descriptive Name; allows Descriptive Names for IOBoxes which don't refer to DOM elements

nodes/vvvv.nodes.value.js
@@ -359,12 +359,15 @@ VVVV.Nodes.IOBoxValueAdvanced = function(id, graph) {
   this.addOutputPin("Y Output Value", [0.0], this);
 
   this.evaluate = function() {
-    if (this.inputPins["Y Input Value"].pinIsChanged()) {
-      for (var i=0; i<this.inputPins["Y Input Value"].values.length; i++) {
-        this.outputPins["Y Output Value"].setValue(i, parseFloat(this.inputPins["Y Input Value"].values[i]));
-      }
-      this.outputPins["Y Output Value"].setSliceCount(this.inputPins["Y Input Value"].getSliceCount());
+    var sliceCount = this.inputPins["Y Input Value"].getSliceCount();
+    var v;
+    for (var i=0; i<sliceCount; i++) {
+      v = parseFloat(this.inputPins["Y Input Value"].getValue(0));
+      if (!isFinite(v))
+        v = 0.0;
+      this.outputPins["Y Output Value"].setValue(i, v);
     }
+    this.outputPins["Y Output Value"].setSliceCount(this.inputPins["Y Input Value"].getSliceCount());
   }
 
 }

editors/vvvv.editors.browser_editor.js
@@ -493,7 +493,6 @@ VVVV.Editors.BrowserEditor.PatchWindow = function(p, editor) {
         thatWin.state = UIState.AreaSelecting;
         selectionBB.x1 = selectionBB.x2 = d3.event.pageX+1;
         selectionBB.y1 = selectionBB.y2 = d3.event.pageY+1;
-        console.log(d3.event.pageY);
         
         chart.append('svg:rect')
           .attr('class', 'selection-area')
@@ -673,13 +672,29 @@ VVVV.Editors.BrowserEditor.PatchWindow = function(p, editor) {
       .attr('fill', function(d) { return d.isIOBox? "#dddddd" : "#9a9a9a"; })
       .attr('width', function(d) { return d.getWidth(); })
       
+    nodes.append('svg:g')
+      .attr('class', 'descriptive-name-bg')
+      
     nodes.append('svg:text')
       .text(function(d) { return (d.invisiblePins["Descriptive Name"]) ? d.invisiblePins["Descriptive Name"].getValue(0) : null })
       .attr('class', 'vvvv-node-descriptive-name')
       .attr('shape-rendering', 'crispEdges')
       .attr('dy', function(d) { return d.getHeight()+12 })
+      .attr('dx', 2)
       .attr('font-size', 10)
       .attr('font-family', 'Lucida Sans Unicode')
+      .attr('fill', 'white')
+      
+    nodes.selectAll('g.descriptive-name-bg')
+      .append('svg:rect')
+        .attr('fill', function(d) {
+          if (d.invisiblePins["Descriptive Name"]==undefined || d.invisiblePins["Descriptive Name"].getValue(0)=="")
+            return 'rgba(0,0,0,0)';
+          return 'rgba(0,0,0,1)'; 
+        })
+        .attr('y', function(d) { return d.getHeight() })
+        .attr('width', 5)
+        .attr('height', 14)
       
     nodes.each(function(d) {
       if (d.isIOBox && VVVV.PinTypes[d.IOBoxInputPin().typeName].makeLabel) {
@@ -849,7 +864,11 @@ VVVV.Editors.BrowserEditor.PatchWindow = function(p, editor) {
       .attr('y1', function(d) { return d.fromPin.y + d.fromPin.node.y + 4 + .5 })
       .attr('x2', function(d) { return d.toPin.x + d.toPin.node.x + 2 + .5 })
       .attr('y2', function(d) { return d.toPin.y + d.toPin.node.y + .5 });
-    //$('.vvvv-link', this.window.document).insertAfter($('.chart>rect', this.window.document)); // move links to the top, to get the right drawing order
+    
+    // set descriptive name widths after rendering
+    $('.descriptive-name-bg rect', thatWin.window.document).each(function() {
+      $(this, thatWin.window.document).attr('width', $(this, thatWin.window.document).parent().next().get(0).getBBox().width+4);
+    })
     
     // Editing Functionality starts here ...
     

mainloop/vvvv.dominterface.js
@@ -29,6 +29,8 @@ VVVV.Core.DOMInterface = function(patch) {
       'values': [],
       'node': node
     }
+    if ($(ioboxConn.selector).length == 0)
+      return;
     if (node.getUpstreamNodes().length==0)
       inputConnectors[match[0]] = ioboxConn;
     else if (node.getDownstreamNodes().length==0)

commit bc6d8497acd67c688f559434944be86347c39cb0

fixes bug in IOBox (Value) where only the first slice was output correctly
caused by b6cdee2ed6570c192d7ff53a035f474b737c107e

core/vvvv.core.js
@@ -1117,12 +1117,12 @@ VVVV.Core = {
             });
           }
           else {
-            //try {
+            try {
               node.evaluate();
-            //}
-            //catch (e) {
-              //console.log('VVVV.Js / Error evaluating '+node.nodename+': '+e.message);
-            //}
+            }
+            catch (e) {
+              console.log('VVVV.Js / Error evaluating '+node.nodename+': '+e.message);
+            }
             if (print_timing)
               console.log(node.nodename+' / '+node.id+': '+(new Date().getTime() - start)+'ms')
             node.dirty = false;

nodes/vvvv.nodes.value.js
@@ -362,7 +362,7 @@ VVVV.Nodes.IOBoxValueAdvanced = function(id, graph) {
     var sliceCount = this.inputPins["Y Input Value"].getSliceCount();
     var v;
     for (var i=0; i<sliceCount; i++) {
-      v = parseFloat(this.inputPins["Y Input Value"].getValue(0));
+      v = parseFloat(this.inputPins["Y Input Value"].getValue(i));
       if (!isFinite(v))
         v = 0.0;
       this.outputPins["Y Output Value"].setValue(i, v);