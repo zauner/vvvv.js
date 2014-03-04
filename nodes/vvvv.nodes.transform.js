8b89b3a4870aed895a0906aca0d4bce1d1ca302c
zauner <sagishi@zive.at>
2014-03-04 22:39:41 +0100

    adds LookAt (Transform) and LookAt (Transform Vector)

--- nodes/vvvv.nodes.transform.js
+++ nodes/vvvv.nodes.transform.js
@@ -266,3 +266,125 @@ VVVV.Nodes.InverseTransform = function(id, graph) {
 
 }
 VVVV.Nodes.InverseTransform.prototype = new VVVV.Core.Node();
+
+
+/*
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ NODE: LookAt (Transform Vector)
+ Author(s): 'Matthias Zauner'
+ Original Node Author(s): 'VVVV Group'
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+*/
+
+VVVV.Nodes.LookAtTransformVector = function(id, graph) {
+  this.constructor(id, "LookAt (Transform Vector)", graph);
+  
+  this.meta = {
+    authors: ['Matthias Zauner'],
+    original_authors: ['VVVV Group'],
+    credits: [],
+    compatibility_issues: []
+  };
+  
+  this.auto_evaluate = false;
+  
+  var transformIn = this.addInputPin('Transform In', [], VVVV.PinTypes.Transform);
+  var positionxyzIn = this.addInputPin('Position XYZ', [0, 0, 0], VVVV.PinTypes.Value);
+  var lookatxyzIn = this.addInputPin('LookAt XYZ', [0, 0, 1], VVVV.PinTypes.Value);
+  var upvectorxyzIn = this.addInputPin('UpVector XYZ', [0, 1, 0], VVVV.PinTypes.Value);
+
+  var transformoutOut = this.addOutputPin('Transform Out', [], VVVV.PinTypes.Transform);
+  
+  var mirror = mat4.create();
+  mat4.identity(mirror);
+  mat4.scale(mirror, [-1, 1, -1]);
+
+  this.evaluate = function() {
+    var spreadCount = Math.max(transformIn.getSliceCount(), Math.max(positionxyzIn.getSliceCount()/3, Math.max(lookatxyzIn.getSliceCount()/3, upvectorxyzIn.getSliceCount()/3)));
+    spreadCount = Math.ceil(spreadCount);
+    
+    for (var i=0; i<spreadCount; i++) {
+      var pos = positionxyzIn.getValue(i, 3);
+      var lookat = lookatxyzIn.getValue(i, 3);
+      var up = upvectorxyzIn.getValue(i, 3);
+
+      var t = mat4.create();
+      mat4.identity(t);   
+      mat4.lookAt(vec3.create(pos), vec3.create(lookat), vec3.create(up), t);
+      mat4.multiply(mirror, t, t);
+    
+      if (transformIn.isConnected())
+      {
+        mat4.multiply(transformIn.getValue(i), t, t);
+      }
+      
+      transformoutOut.setValue(i, t);
+    }
+    
+    transformoutOut.setSliceCount(spreadCount);
+  }
+
+}
+VVVV.Nodes.LookAtTransformVector.prototype = new VVVV.Core.Node();
+
+
+/*
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ NODE: LookAt (Transform)
+ Author(s): 'Matthias Zauner'
+ Original Node Author(s): 'VVVV Group'
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+*/
+
+VVVV.Nodes.LookAtTransform = function(id, graph) {
+  this.constructor(id, "LookAt (Transform)", graph);
+  
+  this.meta = {
+    authors: ['Matthias Zauner'],
+    original_authors: ['VVVV Group'],
+    credits: [],
+    compatibility_issues: []
+  };
+  
+  this.auto_evaluate = false;
+  
+  // input pins
+  var transformIn = this.addInputPin('Transform In', [], VVVV.PinTypes.Transform);
+  var positionxIn = this.addInputPin('Position X', [0], VVVV.PinTypes.Value);
+  var positionyIn = this.addInputPin('Position Y', [0], VVVV.PinTypes.Value);
+  var positionzIn = this.addInputPin('Position Z', [0], VVVV.PinTypes.Value);
+  var lookatxIn = this.addInputPin('LookAt X', [0], VVVV.PinTypes.Value);
+  var lookatyIn = this.addInputPin('LookAt Y', [0], VVVV.PinTypes.Value);
+  var lookatzIn = this.addInputPin('LookAt Z', [1], VVVV.PinTypes.Value);
+  var upvectorxIn = this.addInputPin('UpVector X', [0], VVVV.PinTypes.Value);
+  var upvectoryIn = this.addInputPin('UpVector Y', [1], VVVV.PinTypes.Value);
+  var upvectorzIn = this.addInputPin('UpVector Z', [0], VVVV.PinTypes.Value);
+
+  var transformoutOut = this.addOutputPin('Transform Out', [], VVVV.PinTypes.Transform);
+
+  var mirror = mat4.create();
+  mat4.identity(mirror);
+  mat4.scale(mirror, [-1, 1, -1]);
+
+  this.evaluate = function() {
+    var spreadCount = this.getMaxInputSliceCount();
+    
+    for (var i=0; i<spreadCount; i++) {
+      var t = mat4.create();
+      mat4.identity(t);
+      mat4.lookAt(vec3.create([positionxIn.getValue(i), positionyIn.getValue(i), positionzIn.getValue(i)]), vec3.create([lookatxIn.getValue(i), lookatyIn.getValue(i), lookatzIn.getValue(i)]), vec3.create([upvectorxIn.getValue(i), upvectoryIn.getValue(i), upvectorzIn.getValue(i)]), t);
+      mat4.multiply(mirror, t, t);
+    
+      if (transformIn.isConnected())
+      {
+        mat4.multiply(transformIn.getValue(i), t, t);
+      }
+      
+      transformoutOut.setValue(i, t);
+    }
+    
+    transformoutOut.setSliceCount(spreadCount);
+  }
+
+}
+VVVV.Nodes.LookAtTransform.prototype = new VVVV.Core.Node();
