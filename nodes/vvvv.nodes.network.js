fc30b3aa38b8b20a602344a9ec2c5775e91ee52b
Matthias Zauner <sagishi@zive.at>
2016-12-09 09:59:25 +0100

    adds HTTP (Network Post), makes HTTP (Network Get) only fire request when 'Refresh' is banged

--- nodes/vvvv.nodes.network.js
+++ nodes/vvvv.nodes.network.js
@@ -52,7 +52,7 @@ VVVV.Nodes.HTTPGet = function(id, graph) {
   this.evaluate = function() {
     var maxSize = this.getMaxInputSliceCount();
 
-    var pinsChanged = urlIn.pinIsChanged() || nameIn.pinIsChanged() || valueIn.pinIsChanged() || (refreshIn.pinIsChanged() && refreshIn.getValue(0)==1);
+    var pinsChanged = (refreshIn.pinIsChanged() && refreshIn.getValue(0)==1);
 
     if (successOut.getValue(0)==1)
       successOut.setValue(0,0);
@@ -96,6 +96,120 @@ VVVV.Nodes.HTTPGet = function(id, graph) {
 
 }
 VVVV.Nodes.HTTPGet.prototype = new Node();
+
+/*
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ NODE: HTTP (Network Post)
+ Author(s): Matthias Zauner
+ Original Node Author(s): VVVV Group
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+*/
+
+VVVV.Nodes.HTTPPost = function(id, graph) {
+  this.constructor(id, "HTTP (Network Post)", graph);
+
+  this.meta = {
+    authors: ['Matthias Zauner'],
+    original_authors: ['VVVV Group'],
+    credits: [],
+    compatibility_issues: ['Doesnt support proxies','No header output pin yet']
+  };
+
+  this.auto_evaluate = true;
+
+  var urlIn = this.addInputPin("URL", ["http://localhost"], VVVV.PinTypes.String);
+  var nameIn = this.addInputPin("Name", [""], VVVV.PinTypes.String);
+  var valueIn = this.addInputPin("Value", [""], VVVV.PinTypes.String);
+  var fileNameIn = this.addInputPin("File Name", [""], VVVV.PinTypes.String);
+  var fileIn = this.addInputPin("File", ["No File"], VVVV.PinTypes.Node);
+  var refreshIn = this.addInputPin("Refresh", [0], VVVV.PinTypes.Value);
+
+  var statusOut = this.addOutputPin("Status", [""], VVVV.PinTypes.String);
+  var bodyOut = this.addOutputPin("Body", [""], VVVV.PinTypes.String);
+  var progressOut = this.addOutputPin("Progress", [0], VVVV.PinTypes.Value);
+  var failOut = this.addOutputPin("Fail", [0], VVVV.PinTypes.Value);
+  var successOut = this.addOutputPin("Success", [0], VVVV.PinTypes.Value);
+
+  var body;
+  var status;
+  var success;
+  var fail;
+
+  var requestComplete = false;
+
+  var doResetOutPins = -1;
+
+  this.evaluate = function() {
+    var maxSize = this.getMaxInputSliceCount();
+
+    var pinsChanged = (refreshIn.pinIsChanged() && refreshIn.getValue(0)==1);
+
+    if (successOut.getValue(0)==1)
+      successOut.setValue(0,0);
+    if (failOut.getValue(0)==1)
+      failOut.setValue(0, 0);
+
+    if (requestComplete) {
+      bodyOut.setValue(0, body);
+      statusOut.setValue(0, status);
+      successOut.setValue(0, success);
+      failOut.setValue(0, fail);
+      requestComplete = false;
+    }
+
+    if (pinsChanged) {
+      var i = 0;
+      if (urlIn.getValue(i)==undefined) {
+        bodyOut.setValue(0, '');
+        statusOut.setValue(0, '');
+        return;
+      }
+      var formData = new FormData();
+      var paramsCount = Math.max(nameIn.getSliceCount(), valueIn.getSliceCount());
+      for (var i=0; i<paramsCount; i++) {
+        formData.append(nameIn.getValue(i), valueIn.getValue(i));
+      }
+      paramsCount = Math.max(fileNameIn.getSliceCount(), fileIn.getSliceCount());
+      for (var i=0; i<paramsCount; i++) {
+        formData.append(fileNameIn.getValue(i), fileIn.getValue(i));
+      }
+      var xhr = new XMLHttpRequest();
+      xhr.open("POST", urlIn.getValue(0));
+
+      xhr.onload = function(e) {
+        if (xhr.status==200) {
+          body = xhr.responseText;
+          status = xhr.status;
+          success = 1;
+          requestComplete = true;
+        }
+        else {
+          body = xhr.responseText;
+          status = xhr.status;
+          fail = 1
+          requestComplete = true;
+        }
+      }
+
+      xhr.upload.addEventListener("progress", function(e) {
+        progressOut.setValue(0, e.loaded / e.total);
+      });
+
+      xhr.onerror = function(e) {
+        body = '';
+        status = xhr.status;
+        fail = 1;
+        requestComplete = true;
+      }
+
+      xhr.send(formData);
+    }
+  }
+
+}
+VVVV.Nodes.HTTPPost.prototype = new Node();
+
+
 
 VVVV.Nodes.WebsocketClient = function(id, graph) {
   this.constructor(id, "Websocket (Network Client)", graph);
