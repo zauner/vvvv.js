721f03b0c9606700c2ad8d092a826748f044e8d6
Luna Nane <luna@lunanane.com>
2021-04-13 11:31:02 +0200

    audio player working

--- nodes/vvvv.nodes.html5.js
+++ nodes/vvvv.nodes.html5.js
@@ -246,7 +246,7 @@ VVVV.Nodes.FileStreamHTML = function(id, graph) {
   this.constructor(id, "FileStream (HTML5 Div)", graph);
 
   this.meta = {
-    authors: ['David Gann, Matthias Zauner'],
+    authors: ['Luna Nane, Matthias Zauner'],
     original_authors: [],
     credits: [],
     compatibility_issues: []
@@ -863,6 +863,163 @@ VVVV.Nodes.StoreFile = function(id, graph) {
   }
   VVVV.Nodes.Dir.prototype = new Node();
 
+  /*
+  ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+   NODE: AudioPlayer (HTML5 Audio MediaElementSource)
+   Author(s): 'Luna Nane'
+  ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+  */
 
+  VVVV.Nodes.AudioPlayerHTML = function(id, graph) {
+    this.constructor(id, "AudioPlayer (HTML5)", graph);
+
+    this.meta = {
+      authors: ['Luna Nane'],
+      original_authors: [],
+      credits: [],
+      compatibility_issues: []
+    };
+
+    this.auto_evaluate = true;
+    this.environments = ['browser'];
+
+
+    ////////////////////////////////////
+      var networkStates = [ 'NETWORK_EMPTY', 'NETWORK_IDLE', 'NETWORK_LOADING', 'NETWORK_NO_SOURCE' ];
+      var readyStates = [ 'HAVE_NOTHING', 'HAVE_METADATA', 'HAVE_FUTURE_DATA', 'HAVE_ENOUGH_DATA', 'HAVE_CURRENT_DATA' ];
+
+      var playIn = this.addInputPin('Play', [1], VVVV.PinTypes.Value);
+      var loopIn = this.addInputPin('Loop', [0], VVVV.PinTypes.Value);
+      var startTimeIn = this.addInputPin('Start Time', [0.0], VVVV.PinTypes.Value);
+      var endTimeIn = this.addInputPin('End Time', [-1.0], VVVV.PinTypes.Value);
+      var doSeekIn = this.addInputPin('Do Seek', [0], VVVV.PinTypes.Value);
+      var seekPosIn = this.addInputPin('Seek Position', [0.0], VVVV.PinTypes.Value);
+      var filenameIn = this.addInputPin('Filename', ['http://html5doctor.com/demos/video-canvas-magic/video.ogg'], VVVV.PinTypes.String);
+      var volumeIn = this.addInputPin('Volume', [1], VVVV.PinTypes.Value);
+
+      var durationOut = this.addOutputPin('Duration', [0.0], VVVV.PinTypes.Value);
+      var positionOut = this.addOutputPin('Position', [0.0], VVVV.PinTypes.Value);
+      var networkStatusOut = this.addOutputPin('Network Status', [''], VVVV.PinTypes.String);
+      var readyStatusOut = this.addOutputPin('Ready Status', [''], VVVV.PinTypes.String);
+
+      var videos = [];
+
+
+    ///////////////////////////////////////
+
+
+    this.initialize = function() {};
+
+    var mediaElements = [];
+
+    this.evaluate = function()
+    {
+      /////////////////////////////////Filestream///////////
+
+      var maxSpreadSize = this.getMaxInputSliceCount();
+
+      if (filenameIn.pinIsChanged()) {
+        for (var i=0; i<maxSpreadSize; i++) {
+          filename = VVVV.Helpers.prepareFilePath(filenameIn.getValue(i), this.parentPatch);
+          if (videos[i]==undefined) {
+            var $video = $('<audio loop><source src="" type="audio/mp3">   </audio>');
+            $('body').append($video);
+            videos[i] = $video[0];
+            videos[i].volume = 1;
+            var updateStatus = (function(j) {
+              return function() {
+
+              }
+            })(i);
+            videos[i].onprogress = updateStatus;
+            videos[i].oncanplay = updateStatus;
+            videos[i].oncanplaythrough = updateStatus;
+          }
+          if (filename!=videos[i].currentSrc) {
+            $(videos[i]).find('source').first().attr('src', filename);
+            videos[i].load();
+            if (playIn.getValue(i)>0.5){
+              videos[i].play();
+              console.log("playing");
+              }
+            else{
+              videos[i].pause();
+              }
+            videos[i].loaded = true;
+
+          }
+        }
+      }
+
+      if (volumeIn.pinIsChanged()) {
+        for (var i=0; i<maxSpreadSize; i++) {
+          videos[i].volume = volumeIn.getValue(i);
+        }
+      }
+
+      if (playIn.pinIsChanged()) {
+        for (var i=0; i<maxSpreadSize; i++) {
+          if (playIn.getValue(i)>0.5){
+            videos[i].play();
+            videos[i].volume = 1;
+            }
+          else{
+            videos[i].pause();
+            }
+        }
+      }
+
+      if (doSeekIn.pinIsChanged()) {
+        for (var i=0; i<maxSpreadSize; i++) {
+          if (videos[i%videos.length].loaded && doSeekIn.getValue(i)>=.5) {
+            videos[i%videos.length].currentTime = seekPosIn.getValue(i);
+            if (playIn.getValue(i)>.5)
+              videos[i].play();
+          }
+        }
+      }
+
+      for (var i=0; i<maxSpreadSize; i++) {
+
+
+        if (loopIn.getValue(i)>=.5){
+          videos[i].createAttribute("loop");
+          }
+        else{
+          videos[i].removeAttribute("loop");
+          }
+
+        if (!videos[i].paused) {
+          if (durationOut.getValue(i)!=videos[i].duration)
+            durationOut.setValue(i, videos[i].duration);
+            positionOut.setValue(i, videos[i].currentTime);
+          var endTime = endTimeIn.getValue(i);
+          var startTime = startTimeIn.getValue(i);
+          if (videos[i].currentTime<startTime)
+            videos[i].currentTime = startTime;
+          if (videos[i].currentTime>=videos[i].duration || (endTime>=0 && videos[i].currentTime>=endTime)) {
+            if (loopIn.getValue(i)>=.5){
+              console.log("trying to loop")
+              videos[i].currentTime = startTime;
+              }
+            else{
+              videos[i].pause();
+              }
+          }
+        }
+
+        if (networkStatusOut.getValue(i)!=networkStates[videos[i].networkState])
+          networkStatusOut.setValue(i, networkStates[videos[i].networkState]);
+        if (readyStatusOut.getValue(i)!=readyStates[videos[i].readyState])
+          readyStatusOut.setValue(i, readyStates[videos[i].readyState]);
+      }
+
+
+      //////////////////////////////////////////////////////////////////////////////////////////////
+
+
+    }
+  }
+  VVVV.Nodes.AudioPlayerHTML.prototype = new Node();
 
 });
